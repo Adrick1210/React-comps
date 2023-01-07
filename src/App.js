9fb2b7f0997133394adb0cb4f3e89da161aacf16
Christian Alexander <christianalexander@christians-mbp.lan>
2023-01-06 16:33:48 -0800

    first commmit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+
+function App() {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button
+          secondary
+          rounded
+          outline
+          className="mb-5"
+          onClick={handleClick}
+        >
+          <GoBell />
+          Click me!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          See Deals!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Checkout!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
