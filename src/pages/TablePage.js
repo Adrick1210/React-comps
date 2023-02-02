e23f7daeb23e5ac4f26615206c73ce44f0c7b6ff
Christian Alexander <christianalexander@christians-mbp.lan>
2023-02-02 14:25:18 -0800

    2/2

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -10,27 +10,29 @@ function TablePage() {
   ];
 
   const config = [
-    { 
-        label: "Fruits", 
-        render: (fruit) => fruit.name 
-    }, 
-    { 
-        label: "Color", 
-        render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div> 
-    }, 
-    { 
-        label: "Score", 
-        render: (fruit) => fruit.score, 
-        /* sort: (a, b) => */ }
-];
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
+  ];
 
-const keyFn = (fruit) => {
-   return fruit.name;
-};
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
